f08f4dc4b1be8b0d6923e647d4e9ced90f2fb93d
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2023-11-15 14:18:31 +0700

    Reformat

--- setGPU.py
+++ setGPU.py
@@ -5,14 +5,18 @@ import sys
 import gpustat
 import random
 
-stats = gpustat.GPUStatCollection.new_query()
-ids = map(lambda gpu: int(gpu.entry['index']), stats)
-ratios = map(lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total']), stats)
-pairs = list(zip(ids, ratios))
-random.shuffle(pairs)
-bestGPU = min(pairs, key=lambda x: x[1])[0]
 
-print(f'Setting GPU to {bestGPU}', file=sys.stderr)
-os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = str(bestGPU)
-print(f"export CUDA_VISIBLE_DEVICES={bestGPU}")
+def set_gpu():
+    stats = gpustat.GPUStatCollection.new_query()
+    ids = map(lambda gpu: int(gpu.entry["index"]), stats)
+    ratios = map(
+        lambda gpu: float(gpu.entry["memory.used"]) / float(gpu.entry["memory.total"]),
+        stats,
+    )
+    pairs = list(zip(ids, ratios))
+    random.shuffle(pairs)
+    bestGPU = min(pairs, key=lambda x: x[1])[0]
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(bestGPU)
+    print(f"export CUDA_VISIBLE_DEVICES={bestGPU}")
--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='setGPU',
-    version='0.0.8',
+    name="setGPU",
+    version="0.0.9",
     description="A small Python library that automatically sets CUDA_VISIBLE_DEVICES to the least-loaded GPU on multi-GPU systems.",
-    author='Brandon Amos',
-    author_email='bamos@cs.cmu.edu',
-    platforms=['any'],
+    author="Brandon Amos",
+    author_email="datthinh1801@gmail.com",
+    platforms=["any"],
     license="Public Domain",
-    url='https://github.com/bamos/setGPU',
-    py_modules=['setGPU'],
-    install_requires=[
-        'gpustat'
-    ]
+    url="https://github.com/datthinh1801/setGPU",
+    py_modules=["setGPU"],
+    install_requires=["gpustat"],
 )
