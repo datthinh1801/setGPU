236b5a8ea4ccac30cda995df7f57b960674cfac6
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2023-11-15 15:39:03 +0700

    Refactor

--- setgpu/setgpu.py
+++ setgpu/setgpu.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import gpustat
 import random
 
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="setgpu",
-    version="0.0.9",
+    version="0.1.0",
     description="A small Python library that automatically sets CUDA_VISIBLE_DEVICES to the least-loaded GPU on multi-GPU systems.",
-    author="Brandon Amos",
+    author="datthinh1801",
     author_email="datthinh1801@gmail.com",
     platforms=["any"],
     license="Public Domain",
     url="https://github.com/datthinh1801/setGPU",
     py_modules=["setgpu"],
     install_requires=["gpustat"],
+    packages=find_packages(),
 )
