338a333d3cbecd6214d8c85eb1db340265fa2a66
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2023-12-14 10:21:22 +0700

    Update to hide insufficient power GPU

--- setgpu/setgpu.py
+++ setgpu/setgpu.py
@@ -7,13 +7,17 @@ def set_gpu():
     stats = gpustat.GPUStatCollection.new_query()
     ids = map(lambda gpu: int(gpu.entry["index"]), stats)
     ratios = map(
-        lambda gpu: float(gpu.entry["memory.used"]) / float(gpu.entry["memory.total"]),
+        lambda gpu: gpu.memory_free * int(gpu.power_limit - gpu.power_draw > 100),
         stats,
     )
-    pairs = list(zip(ids, ratios))
+    pairs = [(id_, ratio) for id_, ratio in zip(ids, ratios) if ratio > 0]
     random.shuffle(pairs)
-    bestGPU = min(pairs, key=lambda x: x[1])[0]
-
-    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(bestGPU)
-    print(f"export CUDA_VISIBLE_DEVICES={bestGPU}")
+    try:
+        bestGPU = min(pairs, key=lambda x: x[1])[0]
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(bestGPU)
+        print(f"export CUDA_VISIBLE_DEVICES={bestGPU}")
+    except ValueError:
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+        print(f"No GPU available")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="setgpu",
-    version="0.1.0",
+    version="0.1.1",
     description="A small Python library that automatically sets CUDA_VISIBLE_DEVICES to the least-loaded GPU on multi-GPU systems.",
     author="datthinh1801",
     author_email="datthinh1801@gmail.com",
