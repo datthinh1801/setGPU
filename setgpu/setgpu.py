fd5365aebb57d202787530a21b539b052831a1b5
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2023-12-16 09:01:39 +0700

    Check mem_free only

--- setgpu/setgpu.py
+++ setgpu/setgpu.py
@@ -6,18 +6,12 @@ import random
 def set_gpu():
     stats = gpustat.GPUStatCollection.new_query()
     ids = map(lambda gpu: int(gpu.entry["index"]), stats)
-    ratios = map(
-        lambda gpu: gpu.memory_free * int(gpu.power_limit - gpu.power_draw > 100),
-        stats,
-    )
-    pairs = [(id_, ratio) for id_, ratio in zip(ids, ratios) if ratio > 0]
+    ratios = map(lambda gpu: gpu.memory_free, stats)
+
+    pairs = list(zip(ids, ratios))
     random.shuffle(pairs)
-    try:
-        bestGPU = min(pairs, key=lambda x: x[1])[0]
-        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(bestGPU)
-        print(f"export CUDA_VISIBLE_DEVICES={bestGPU}")
-    except ValueError:
-        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"] = ""
-        print(f"No GPU available")
+    bestGPU = max(pairs, key=lambda x: x[1])[0]
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(bestGPU)
+    print(f"export CUDA_VISIBLE_DEVICES={bestGPU}")
